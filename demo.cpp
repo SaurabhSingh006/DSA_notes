67f79af8045d3cfa8c7cd197133e36bd9bf4bcc2
Saurabh Singh <saurabhsinghsix@gmail.com>
2024-06-21 20:07:50 +0530

    Initial commit

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class student {
+    string name;
+    int age;
+    bool gender;
+
+    
+}
+
+int main() { 
+
+    cout << "Main ends" << endl << endl;
+}
