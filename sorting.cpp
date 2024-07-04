6d3ce01b091f40f6b23b9f6bb729e0eb6b26c8fa
Saurabh Singh <saurabhsinghsix@gmail.com>
2024-07-04 22:47:06 +0530

    Added merge sorting concept

--- sorting.cpp
+++ sorting.cpp
@@ -5,6 +5,13 @@ void displayVecor(vector<int> vec) {
     for(int i: vec) {
         cout << i << "=>";
     }
+    cout << endl;
+}
+
+void swap(int &a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
 }
 
 vector<int> selectionSorting(vector<int> inputArr) {
@@ -42,28 +49,46 @@ vector<int> bubbleSorting(vector<int> inputArr) {
 
 vector<int> insertionSorting(vector<int> inputArr) {
     for(int i = 0; i < inputArr.size(); i++) {
-        // cout << inputArr[i] << endl;
-        cout << inputArr[i] << "emement" << endl;
-        int j = i - 1;
-        while (j >= 0)
+        int j = i;
+        while (j > 0)
         {
-           cout << inputArr[j] << " " << inputArr[i] << " j\n";
-           if(inputArr[j] > inputArr[j+1]) {
-            int temp = inputArr[j];
-            inputArr[j] = inputArr[j+1];
-            inputArr[j+1] = temp;
-           }else break;
-           j--;
+            if(inputArr[j-1] > inputArr[j]) {
+                int temp = inputArr[j];
+                inputArr[j] = inputArr[j-1];
+                inputArr[j-1] = temp;
+            } else break;
+            j--;
         }
-    
     }
+    return inputArr;
+}
 
+vector<int> mergeSorting(vector<int> &inputArr, int start, int end) {
+    if(end - start > 2) {
+        int mid = (start + end) % 2 ? (start + end + 1) / 2 : (start + end) / 2;
+        mergeSorting(inputArr, start, mid);
+        mergeSorting(inputArr, mid, end);
+
+        for(int i = 0; i <= mid; i++) {
+            int j = 0;
+            while (j < (end - mid))
+            {
+                if(inputArr[i] > inputArr[mid + j]) swap(inputArr[i], inputArr[mid + j]);
+                j++;
+            }
+            
+        }
+    } else {
+        if(inputArr[start] > inputArr[end - 1]) swap(inputArr[start], inputArr[end - 1]);
+    }
+     
     return inputArr;
 }
 
 int main() { 
     cout << "Sorting" << endl;
-    vector<int> arr = {52, 58, -1, 78, 69, 23};
+    vector<int> arr = {52, 58, -1, 78, 69, 23, 5, 6, 45, 574, 87, 54, 51, 12};
+    // arr = {56,2,8,45, 5, 89};
 
     // Method 1 => Sorting using selection sorting method.
     // SELECTION SORTING => Find the minimum number and swap the postion
@@ -75,7 +100,7 @@ int main() {
     // Time complexity ==> n+(n-1)+(n-2)+ ... + 1 = n(n-1)/2 ==> n^2
     // So Best/Worst/Average ==> O(n^2)
 
-    vector<int> res1 = selectionSorting(arr);
+    // vector<int> res1 = selectionSorting(arr);
 
     // Method 2 => Sorting using bubble sorting method.
     // Bubble SORTING => Find the minimum number and swap the postion
@@ -84,7 +109,7 @@ int main() {
     // So Worst/Average ==> O(n^2)
     // Best is o(n)
 
-    vector<int> res2 = bubbleSorting(arr);
+    // vector<int> res2 = bubbleSorting(arr);
 
     // Method 3 ==> Insertion sort
     // Comparing current element with previous element and moving towards the left until the proper postion is achieved.
@@ -92,10 +117,13 @@ int main() {
     // So Worst/Average ==> O(n^2)
     // Best is o(n)
 
-    vector<int> res3 = insertionSorting(arr);
+    // vector<int> res3 = insertionSorting(arr);
 
-    displayVecor(res3);
+    // Method 4 ==> Merge sorting
+    // Also knows as divide and conquer method
+    vector<int> res4 = mergeSorting(arr, 0, arr.size());
 
+    displayVecor(arr);
 
     cout << endl << endl;
 }
